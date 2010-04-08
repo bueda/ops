1de787703b06daa4dfd13d66512d4204a9498af1
Christopher Peplin <peplin@bueda.com>
2010-04-08 17:28:06 -0400

    Fix and test re-chef deployment tool.

--- fabfile.py
+++ fabfile.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python
 import os
-import boto
-from fabric.api import *
+from fabric.api import env, abort, require, settings, runs_once, prompt, get
+from fabric.contrib.console import confirm
 from fab_shared import (_find_unit_root, _development, _production, _clone,
-        _make_release, _upload_to_s3, restart_webserver, rollback)
+        _make_release, TIME_NOW, _make_archive, _conditional_upload_to_s3,
+        S3_KEY, local, put, run, sudo, EC2_CONNECTION, ELB_CONNECTION)
+import time
 
 env.unit = "chef"
 env.user_data = "deployment/chef.user-data"
+env.scm = "git@github.com:bueda/chef"
 env.root_dir = _find_unit_root(os.path.abspath(os.path.dirname(__file__)))
+
+env.scratch_path = '/tmp/%s-%s' % (env.unit, TIME_NOW)
 
 def development():
     """
     [Env] Sets environment for development server.
     """
     _development()
+    env.tagged = False
     env.security_groups = ["development", "ssh", "database-client"]
     env.key_name = "development"
-    env.chef_configs = ["common.json", "common-web.json", "dev.json",
-            "lda.json", "solr.json"]
+    env.chef_configs = ["common", "common-web", "dev",
+            "lda", "solr"]
 
 def production():
     """
     [Env] Sets environment for production servers behind load balancer.
     """
     _production()
+    env.tagged = False
     env.security_groups = ["production", "ssh", "database-client"]
     env.key_name = "production"
-    env.chef_configs = ["common.json", "common-web.json", "production.json"]
+    env.chef_configs = ["common", "common-web", "production"]
 
 def deploy(release=None):
     """
@@ -35,13 +42,15 @@ def deploy(release=None):
     require('hosts', provided_by = [development, production])
     require('unit')
 
-    env.scratch_path = '/tmp/%(unit)s' % env
-    _clone()
-    if test():
-        abort("Tests did not pass")
+    _clone(release)
     _make_release(release)
-    if confirm("Upload to S3?", default=env.upload_to_s3):
-        _upload_to_s3()
+    require('pretty_release')
+    require('archive')
+    if test(env.scratch_path):
+        abort("Unit tests did not pass")
+    require('pretty_release')
+
+    _conditional_upload_to_s3()
     if confirm("Re-Chef?", default=True):
         rechef(release=env.release)
 
@@ -50,9 +59,10 @@ def rechef(release=None):
     Run the latest commit of the Chef cookbook on all servers.
     """
     require('chef_configs', provided_by=[development, production])
-    require('unit')
-    if (not release
-            and confirm("Re-chef with production cookbook?", default=True)):
+    require('tagged')
+    archive_path = '/tmp/chef-%s.tar.gz' % TIME_NOW
+    if (not env.tagged and
+            confirm("Re-chef with production cookbook?", default=True)):
         S3_KEY.key = '%(unit)s.tar.gz' % env
         S3_KEY.get_contents_to_filename('/tmp/%(unit)s.tar.gz' % env)
     else:
@@ -60,22 +70,24 @@ def rechef(release=None):
             env.release = prompt("Chef commit or tag?", default='HEAD')
         _clone()
         _make_archive()
-        local('mv /tmp/%(release)s-%(unit)s.tar.gz /tmp/%(unit)s.tar.gz' % env)
-    put('/tmp/%(unit)s.tar.gz' % env, '/tmp/', mode=0777)
-    run('rm -rf %(scratch_path)s*' % env)
-    run('tar -xzf /tmp/%(unit)s.tar.gz -C /tmp' % env)
+        require('scratch_path')
+        require('archive')
+        local('mv %s/%s %s' % (env.scratch_path, env.archive, archive_path))
+    put(archive_path, '/tmp', mode=0777)
+    run('tar -xzf %s -C /tmp' % archive_path)
     _run_chef_solo('base')
     for config in env.chef_configs:
         _run_chef_solo(config)
-    local('rm -rf %(scratch_path)s*' % env)
+    run('rm -rf /tmp/%(unit)s' % env)
 
 def _run_chef_solo(config):
-    with settings(config=config, warn_only=True):
+    env.config = config
+    with settings(warn_only=True):
         result = sudo("""
-            cd %(scratch_path)s;
+            cd /tmp/%(unit)s;
             /var/lib/gems/1.8/bin/chef-solo \
-                -j %(scratch_path)s/config/%(config)s \
-                -c %(scratch_path)s/config/solo.rb
+                -j /tmp/%(unit)s/config/%(config)s.json \
+                -c /tmp/%(unit)s/config/solo.rb
             """ % env)
     if result.failed:
         abort("Chef run failed, %s" % result)
@@ -92,7 +104,7 @@ def spawn_ec2_instance():
     require('key_name', provided_by=[production, development])
     print "Launching instance with image %s" % env.ami
 
-    image = ec2_connection.get_image(env.ami)
+    image = EC2_CONNECTION.get_image(env.ami)
     print "Found AMI image image %s" % image
 
     user_data_file = open(env.user_data, "rb").read()
@@ -124,13 +136,13 @@ def spawn_ec2_instance():
     if (env.key_name == "production"
             and confirm("Attach to load balancer? Test before saying yes!",
             default=False)):
-        status = elb_connection.register_instances(env.load_balancer,
+        status = ELB_CONNECTION.register_instances(env.load_balancer,
                 [instance.id])
         print("Status of attaching %s to load balancer %s was %s" 
                 % (instance.id, env.load_balancer, status))
 
-def test():
-    require('scratch_path')
-    require('cloned')
-    with cd(env.scratch_path):
+def test(dir=None):
+    if not dir:
+        dir = env.root_dir
+    with settings(root_dir=dir):
         return local('rake' % env, capture=False).return_code
